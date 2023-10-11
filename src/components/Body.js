77f35a9670bf3f0d04c151865ff54001408f60d1
ayushb02 <ayushbhadoria14@gmail.com>
2023-10-11 23:06:31 +0530

    fetching data from swiggi api and made the top rated button functional

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,60 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Rest_card from "./Rest_card";
 
 function Body() {
+  const [isChecked, setIschecked] = useState(false);
+
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.2124007&lng=78.1772053&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log("json");
+
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <img className="loading" src="https://i.gifer.com/PYh.gif" />;
+  };
+  
+
   return (
     <div className="body">
       <div className="toprated-btn-container">
-      <input type="checkbox" id="switch" /><label for="switch">Toggle</label><span id="top-label">Top Rated Restaurant</span>
+        <input
+          type="checkbox"
+          id="switch"
+          onChange={() => {
+            setIschecked(!isChecked);
+
+            if (!isChecked) {
+              const newlist = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setListOfRestaurants(newlist);
+            } else {
+              fetchData();
+            }
+          }}
+        />
+        <label for="switch">Toggle</label>
+        <span id="top-label">Top Rated Restaurant</span>
       </div>
-      <Rest_card
-        resName="Meghna Foods"
-        imgSrc="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        cuisine="Indian, Chinese"
-        rating="3.5"
-      />
-      <Rest_card
-        resName="La Pino'z Pizza"
-        imgSrc="https://b.zmtcdn.com/data/pictures/chains/8/20284898/eac5d48527700f6ea1bcfbb2094456d0.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
-        cuisine="Italian, Fast Food"
-        rating="4.5"
-      />
-      <Rest_card
-        resName="Shere Punjab"
-        imgSrc="https://b.zmtcdn.com/data/pictures/0/20127450/d6b0bb908e07fa829dd79e31a0ec9588.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
-        cuisine="North Indian"
-        rating="3.5"
-      />
-      <Rest_card
-        resName="Chicagi Pizza"
-        imgSrc="https://b.zmtcdn.com/data/pictures/3/20390213/c23c404948060cfcebd45c330ef3866b_o2_featured_v2.jpg"
-        cuisine="Pizza, Fast Food"
-        rating="3.5"
-      />
-      <Rest_card
-        resName="Momo Adda"
-        imgSrc="https://b.zmtcdn.com/data/pictures/3/20203643/be8b90f87bf9c59ed2e5bdc433a7ea92_o2_featured_v2.jpg"
-        cuisine="Fast Food, Momos"
-        rating="5"
-      />
-      <Rest_card
-        resName="Belgian Waffle"
-        imgSrc="https://b.zmtcdn.com/data/pictures/chains/6/19873946/bd6d35499c5fb33bea73c32bbf4b0237_o2_featured_v2.jpg"
-        cuisine="Waffle, Pancake"
-        rating="4"
-      />
-      <Rest_card
-        resName="The Delight"
-        imgSrc="https://b.zmtcdn.com/data/pictures/1/20492391/3d6d15fb0172ef1f5dfa3ad9de55098e_o2_featured_v2.jpg"
-        cuisine="Indian, Burger"
-        rating="3.5"
-      />
-      <Rest_card
-        resName="Biryani By Kilo"
-        imgSrc="https://b.zmtcdn.com/data/pictures/chains/7/20293527/1e4be0b5e0f970aef41328752d9c4bc1_o2_featured_v2.jpg"
-        cuisine="Biryani, Mughal"
-        rating="3.5"
-      />
+      {listOfRestaurants.map((restaurants) => (
+        <Rest_card key={restaurants.info.id} resData={restaurants} />
+      ))}
     </div>
   );
 }
--- src/components/Rest_card.js
+++ src/components/Rest_card.js
@@ -1,25 +1,31 @@
 import React from "react";
 
 function Rest_card(props) {
-    const{ resName , imgSrc , cuisine, rating} = props;
   
-    return (
+  const { resData } = props;
+
+  const { name, cloudinaryImageId, avgRating, cuisines } = resData?.info;
+
+  return (
     <div className="rest-card">
       <img
         className="res-logo"
         alt="https://www.cypressgreen.in/blog/wp-content/uploads/2021/03/food.jpg"
-        src={imgSrc}
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
       />
-      <h3>{resName}</h3>
+      <h3 className="res-name">{name}</h3>
       <div>
-        <span className="rating">{rating}</span>
+        <span className="rating">{avgRating}</span>
         <img
           className="rate-logo"
           src="https://static.thenounproject.com/png/766721-200.png"
         />
       </div>
       <div className="res-dis">
-        <h4 className="cuisine">Cuisine :- {cuisine}</h4>
+        <h4 className="cuisine">Cuisine :- {cuisines.join(", ")}</h4>
       </div>
     </div>
   );
